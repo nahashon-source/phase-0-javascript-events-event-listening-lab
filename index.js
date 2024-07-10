d92c476d45bbb9854349f0063383144bb0ec9497
nahashon <nashonmwendwa0@gmail.com>
2024-07-10 12:02:52 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const button = document.getElementById('myButton');
+    
+    button.addEventListener('click', function() {
+      alert('Button was clicked!');
+    });
+     input.addEventListener('click',clickAlert);
+  }
+  
+ 
+  addingEventListener();
